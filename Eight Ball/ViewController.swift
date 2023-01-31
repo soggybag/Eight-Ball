832783af0d034c08e3cca9ee252a977732bb06e9
Mitchell <soggybag@gmail.com>
2023-01-30 16:33:30 -0800

    initialize

--- Eight Ball/ViewController.swift
+++ Eight Ball/ViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  
+  @IBOutlet weak var myLabel: UILabel!
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  
+  @IBAction func tapButton(_ sender: Any) {
+    myLabel.text = "Hello World"
+  }
+  
 
 
 }
